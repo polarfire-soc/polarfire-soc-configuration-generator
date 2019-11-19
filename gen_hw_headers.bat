d24ed9f02f5911a948e767b8197578e9e3fed549
Malachy Lynch <Malachy.Lynch@microchip.com>
2019-11-19 17:00:19 +0000

    Changed name of script frpm pg_soc to mpfs_  based on Kevins feedback

--- gen_hw_headers.bat
+++ gen_hw_headers.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
 ECHO generate header files for Embedded software project
-ECHO pfsoc-baremetal-software-cfg-gen.py
-set xml_file=ref_xml/pf_soc_hw_description_reference.xml
-py -3 pfsoc_baremetal_software_cfg_gen.py %xml_file%
+ECHO mpfs-baremetal-software-cfg-gen.py
+set xml_file=ref_xml/mpfs_hw_description_reference.xml
+py -3 mpfs_baremetal_software_cfg_gen.py %xml_file%
--- gen_hw_headers_lin.sh
+++ gen_hw_headers_lin.sh
@@ -1,4 +1,4 @@
 ##!/usr/bin/bash
 echo "generate header files for Embedded software project"
 echo "pfsoc_baremetal_software_cfg_gen.py"
-python3 pfsoc_baremetal_software_cfg_gen.py ref_xml/pf_soc_hw_description_reference.xml
+python3 mpfs_baremetal_software_cfg_gen.py ref_xml/mpfs_hw_description_reference.xml
